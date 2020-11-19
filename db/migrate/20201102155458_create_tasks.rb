2f423500ff5387878a281550b771ed4e2659e1c5
Tony Blum <tonyblum@me.com>
2020-11-19 07:45:57 -0800

    Added Bootstrap and Task badges

---
+++ db/migrate/20201102155458_create_tasks.rb
@@ -0,0 +1,13 @@
+class CreateTasks < ActiveRecord::Migration[6.0]
+  def change
+    create_table :tasks do |t|
+      t.string :title
+      t.string :name
+      t.text :description
+      t.string :status
+      t.references :project, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
